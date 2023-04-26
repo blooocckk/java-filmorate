a0d4d5a187732d5be565994dda76a0abe10ca30f
Ruslan <vkruslan2000@yandex.kz>
2023-04-27 03:17:46 +0500

    style: remove unused imports

--- src/main/java/ru/yandex/practicum/filmorate/validators/ReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/ReleaseDate.java
@@ -5,7 +5,6 @@ import javax.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import java.time.LocalDate;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
